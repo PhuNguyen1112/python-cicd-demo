ffa7405dce658aa5c6f2502efbc5f316b5568a18
phu <phunguyen30072004@gmail.com>
2025-06-05 12:42:26 +0700

    Test CI: 1 đúng 1 sai

--- test_app.py
+++ test_app.py
@@ -1,7 +1,12 @@
 from app import app
 
-def test_home():
+def test_pass():
     client = app.test_client()
     response = client.get('/')
     assert response.status_code == 200
-    assert b"Flask CI/CD" in response.data
+    assert b"Flask CI/CD" in response.data  # ✅ Test đúng
+
+def test_fail():
+    client = app.test_client()
+    response = client.get('/')
+    assert b"Sai noi dung" in response.data  # ❌ Test sai cố ý
