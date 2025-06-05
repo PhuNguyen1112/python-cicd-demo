765bba5708dc2d70b594729b4e810bd7ed488613
phu <phunguyen30072004@gmail.com>
2025-06-05 12:53:50 +0700

    Test CI: test ci/cd  đúng 11

--- test_app.py
+++ test_app.py
@@ -1,12 +1,12 @@
 from app import app
 
-#def test_pass():
- #   client = app.test_client()
- #   response = client.get('/')
- #   assert response.status_code == 200
-  #  assert b"Flask CI/CD" in response.data  # ✅ Test đúng
+def test_pass():
+    client = app.test_client()
+    response = client.get('/')
+    assert response.status_code == 200
+    assert b"Flask CI/CD" in response.data  # ✅ Test đúng
 
-def test_fail():
-   client = app.test_client()
-   response = client.get('/')
-   assert b"Sai noi dung" in response.data  # ❌ Test sai cố ý
+#def test_fail():
+#  client = app.test_client()
+#  response = client.get('/')
+#  assert b"Sai noi dung" in response.data  # ❌ Test sai cố ý
